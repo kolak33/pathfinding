e335bfe4046fc3ac068b95c3a96ea3ee048fe562
Kolakj33 <Kolakj33@gmail.com>
2017-10-30 16:31:03 +0100

    small confix fix

--- pathfinding/Config.h
+++ pathfinding/Config.h
@@ -18,7 +18,7 @@ typedef enum MapCellType {
 	CellTypeShallowWater = 5,
 }MapCellType;
 
- const CString strLoadingFilePrefix = "C:\\Users\\Kuba\\Documents\\Visual Studio 2017\\Projects\\pathfinding\\mapki";
+ const CString strLoadingFilePrefix = "..\\mapki";
 
  const float DIAGONAL_WEIGHT = 1.4142f;
  const float STRAIGHT_WEIGHT = 1.0f;
